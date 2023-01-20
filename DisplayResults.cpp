68d827cecb3752c9751ca9556d6a914bc452606d
amithazan1 <amith246@gmail.com>
2023-01-20 15:54:21 +0200

    DisplayResult class

---
+++ DisplayResults.cpp
@@ -0,0 +1,12 @@
+#include "DisplayResults.h"
+void DisplayResults :: execute() {
+    
+    for(map<int,string>::iterator mit = calssify_data.begin(); mit != calssify_data.end(); mit++)
+    {
+        pair<int,string> data = *mit;
+        IO->write(to_string(data.first));
+        IO->write(" ");
+        IO->write(data.second);
+    }
+    IO->write(".Done");
+}
