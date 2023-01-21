6cac4793c9ba519280fed645e51b84ddeef33ab0
amithazan1 <amith246@gmail.com>
2023-01-21 10:16:40 +0200

    not finshed

---
+++ ClassifyData.h
@@ -0,0 +1,20 @@
+#include "Command.h"
+#include <string>
+#include <vector>
+#include <map>
+#include "KNN.h"
+using namespace std;
+
+class ClassifyData: public Command
+{
+protected:
+    const multimap<vector<double>, string> &database;
+    const vector<vector<double>> &testCSV;
+    int& k;
+    string& distance_metric;
+public:
+    ClassifyData(const multimap<vector<double>, string> &database, const vector<vector<double>> &testCSV,int& k, string& distance_metric );
+    void execute();
+};
+
+
